f3c03ddcdf44ff59218621ad90f7fb00a1a77ef4
Cristian Ionescu <cristian.ionescu202@gmail.com>
2023-09-29 02:57:25 +0200

    started working with spacy

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import json
 from difflib import get_close_matches
+import spacy
+
+# Load the English language model
+nlp = spacy.load("en_core_web_sm")
 
 def load_knowledge_base(file_path: str)-> dict:
     with open(file_path, "r") as file:
-        data: dict = json.loads(file)
+        data: dict = json.load(file)
     return data
 
 def save_knowledge_base(file_path: str, data: dict):
@@ -28,12 +32,28 @@ def chat_bot():
         if user_input.lower() == 'quit':
             break
 
+        doc = nlp(user_input)
+
+        # Access tokens, parts of speech, named entities, etc.
+        for token in doc:
+            print(token.text, token.pos_, token.dep_)
+
         best_match: str | None = find_best_match(user_input, [q["question"] for q in knowledge_base["questions"]])
         if best_match:
             answer: str = get_answer_for_question(best_match, knowledge_base)
             print(f'Bot: {answer}')
         else:
-            print('Bot: I don\'t know the answer. Can you teach me?')
+            for token in doc:
+                if (token.text.lower() == "weather" or token.text.lower() == "tomorrow") and token.dep_ == "nsubj":
+                # User is asking about the weather
+                # Implement logic to fetch weather information and respond
+                    response = "Should I check the weather for you?"
+                    print(f'Bot: {response}')
+                    if user_input.lower() == "yes":
+                        response = "Great! I'll check the weather for you."
+                        print(f'Bot: {response}')
+
+            """print('Bot: I don\'t know the answer. Can you teach me?')
             new_answer: str = input('Type the answer or "skip" to skip: ')
 
             if new_answer.lower() != 'skip':
@@ -42,7 +62,8 @@ def chat_bot():
                 print('Thank you! I learned a new response!')
             else:
                 print('Ok, I\'ll ask you later.')
-                print(knowledge_base)
+                print(knowledge_base)"""
+
 
 if __name__=='__main__':
     chat_bot()      
